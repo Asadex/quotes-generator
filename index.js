963006ac55f87c5a84449816970f443bd9925635
Artur Malisevych <19artur98@gmail.com>
2020-11-17 22:12:53 +0100

    Fixing json fetch...

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 let author = document.getElementById('author')
 let quote = document.getElementById('quote')
-
+//let quotesObject = JSON.parse(quotes)
 let newQuoteButton = document.getElementById("newQuoteButton")
 
 newQuoteButton.addEventListener('click',newQuote)
 
+
+// function newQuote(){
+//     let random = Math.floor(Math.random() * data.length+1)
+        
+//     author.classList.add("fade")
+//     quote.classList.add('fade')
+
+//     author.textContent = '~ ' + data[random].author;
+//     quote.innerHTML =  " &nbsp " + data[random].content
+// }
+
 function newQuote(){
-    fetch("quotes.JSON")
+    let myInit ={
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    let quotes = new Request("https://asadex.github.io/quotes-generator/quotes.json",myInit)
+
+    fetch(quotes)
     .then( res => res.json())
     .then( data => {
         let random = Math.floor(Math.random() * data.length+1)
