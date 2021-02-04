ee015ebb75375b89bd815f226492235485148353
Asadex <19artur98@gmail.com>
2021-02-04 17:21:26 +0100

    Now it uses AJAX

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
 let author = document.getElementById('author')
 let quote = document.getElementById('quote')
-//let quotesObject = JSON.parse(quotes)
 let newQuoteButton = document.getElementById("newQuoteButton")
 
 newQuoteButton.addEventListener('click',newQuote)
 
-
-// function newQuote(){
-//     let random = Math.floor(Math.random() * data.length+1)
-        
-//     author.classList.add("fade")
-//     quote.classList.add('fade')
-
-//     author.textContent = '~ ' + data[random].author;
-//     quote.innerHTML =  " &nbsp " + data[random].content
-// }
-
 function newQuote(){
-    let myInit ={
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        mode: 'cors',
-        cache: 'default'
-    };
-
-    let quotes = new Request("https://asadex.github.io/quotes-generator/quotes.json",myInit)
-
-    fetch(quotes)
-    .then( res => res.json())
+    fetch('https://type.fit/api/quotes')
+    .then( response => response.json())
     .then( data => {
-        let random = Math.floor(Math.random() * data.length+1)
-        
-        // author.classList.add("fade")
-        // quote.classList.add('fade')
+        let random = Math.floor(Math.random() * data.length+ 1);
 
-        author.textContent = '~ ' + data[random].author;
-        quote.innerHTML =  " &nbsp " + data[random].content
-        
+        if(!!data[random].author)
+            author.textContent = '~ ' + data[random].author;
+        else
+            author.textContent = '~ Unknown';
+        quote.innerHTML =  " &nbsp " + data[random].text;
     })
-    // setTimeout(function(){
-    //     author.classList.remove('fade')
-    //     quote.classList.remove('fade')}
-    //     ,500)
 }
